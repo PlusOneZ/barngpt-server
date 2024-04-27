70ffaf26882a9c8cf0626c8aa4666d04d639ea41
Zhengyi Zhuo <zhuozhengyi@icloud.com>
2024-04-27 20:03:29 +0800

    Add first login strategy

---
+++ src/middlewares/passport.middleware.ts
@@ -0,0 +1,157 @@
+import passport from "passport";
+import { Strategy as PassportLocalStrategy } from "passport-local";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { Strategy as FacebookStrategy } from "passport-facebook";
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
+
+import { UserModel } from "../models/user";
+import { loginSchema } from "../utils/validators";
+
+const localLogin = new PassportLocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false,
+        passReqToCallback: true
+    },
+    async (req, email, password, done) => {
+        const { error } = loginSchema.validate(req.body);
+        if (error) {
+            return done(null, false, { message: error.details[0].message });
+        }
+
+        try {
+            const user = await UserModel.findOne({ email: email.trim() });
+            if (!user) {
+                return done(null, false, { message: "Email does not exists"});
+            }
+
+            user.comparePassword(password, function (err, isMatch) {
+                if (err) {
+                    return done(err);
+                }
+                if (!isMatch) {
+                    return done(null, false, { message: "Incorrect password" })
+                }
+
+                return done(null, user)
+            })
+        } catch (err) {
+            return done(err)
+        }
+    }
+)
+
+const secretOrKey = process.env.JWT_SECRET;
+
+const jwtLogin = new JwtStrategy(
+    {
+        jwtFromRequest: ExtractJwt.fromHeader('x-auth-token'),
+        secretOrKey
+    },
+    async (payload, done) => {
+        try {
+            const user = await UserModel.findById(payload.id);
+
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        } catch (err) {
+            done(err, false);
+        }
+    },
+);
+
+const serverUrl = `https://${process.env.HOST}:${process.env.PORT}`
+
+const facebookLogin = new FacebookStrategy(
+    {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: `${serverUrl}${process.env.FACEBOOK_CALLBACK_URL}`,
+        profileFields: [
+            'id',
+            'email',
+            'gender',
+            'profileUrl',
+            'displayName',
+            'locale',
+            'name',
+            'timezone',
+            'updated_time',
+            'verified',
+            'picture.type(large)',
+        ],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            const oldUser = await UserModel.findOne({ email: profile.emails![0].value });
+
+            if (oldUser) {
+                return done(null, oldUser);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+        // register user
+        try {
+            const newUser = new UserModel({
+                provider: 'facebook',
+                facebookId: profile.id,
+                username: `user${profile.id}`,
+                email: profile.emails![0].value,
+                name: profile.displayName,
+                avatar: profile.photos ? profile.photos![0].value : "",
+            });
+            await newUser.save();
+
+            done(null, newUser);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+)
+
+// google strategy
+const googleLogin = new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${serverUrl}${process.env.GOOGLE_CALLBACK_URL}`,
+        proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        // console.log(profile);
+        try {
+            const oldUser = await UserModel.findOne({ email: profile.email });
+
+            if (oldUser) {
+                return done(null, oldUser);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+        try {
+            const newUser = await new UserModel({
+                provider: 'google',
+                googleId: profile.id,
+                username: `user${profile.id}`,
+                email: profile.email,
+                name: profile.displayName,
+                avatar: profile.picture,
+            }).save();
+            done(null, newUser);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+passport.use(localLogin)
+passport.use(jwtLogin)
+passport.use(facebookLogin)
+passport.use(googleLogin)
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,137 @@
 import { Schema, InferSchemaType, model } from "mongoose";
 import { randomUUID } from "crypto"
+import jwt from "jsonwebtoken"
+import bcrypt from 'bcryptjs'
+import Joi from 'joi'
 
 const userSchema = new Schema({
-    _id: { type: Schema.Types.UUID, default: () => randomUUID()},
-    name: { type: String, required: true },
-    email: { type: String, required: true }, // TODO: Checking Format
-    phone: String,
-    avatar: String
+    _id: {
+        type: Schema.Types.UUID,
+        default: () => randomUUID()
+    },
+    provider: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9_]+$/, 'is invalid'],
+        index: true,
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        minlength: 6,
+        maxlength: 60,
+    },
+    name: String,
+    avatar: String,
+    bio: String,
+    // google
+    googleId: {
+        type: String,
+        unique: true,
+        sparse: true,
+    },
+    // fb
+    facebookId: {
+        type: String,
+        unique: true,
+        sparse: true,
+    },
+    // todo: link to tasks
 }, { timestamps: true })
 
 type User = InferSchemaType<typeof userSchema>;
+
+userSchema.methods.toJSON = function () {
+    return {
+        id: this._id,
+        provider: this.provider,
+        email: this.email,
+        username: this.username,
+        avatar: this.avatar,
+        name: this.name,
+        role: this.role,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+    };
+};
+
+const secretOrKey = process.env.JWT_SECRET
+
+userSchema.methods.generateJWT = function () {
+    const token = jwt.sign({
+        expiresIn: '12h',
+        id: this._id,
+        provider: this.provider,
+        email: this.email,
+    }, secretOrKey);
+    return token;
+}
+
+userSchema.methods.registerUser = (newUser, callback: any) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (errh, hash) => {
+            if (err || errh) {
+                console.log(err, errh)
+            }
+            newUser.password = hash;
+            newUser.save(callback)
+        })
+    })
+}
+
+userSchema.methods.comparePassword = function (candidatePassword, callback: any) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+async function hashPassword(password) {
+    const saltRounds = 10;
+
+    const hashedPassword = await new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+            if (err) reject(err);
+            else resolve(hash);
+        });
+    });
+
+    return hashedPassword;
+}
+
+const validateUser = (user) => {
+    const schema = Joi.object({
+        avatar: Joi.any(),
+        name: Joi.string().min(2).max(30).required(),
+        username: Joi.string()
+            .min(2)
+            .max(20)
+            .regex(/^[a-zA-Z0-9_]+$/)
+            .required(),
+        password: Joi.string().min(6).max(20).allow('').allow(null),
+    });
+
+    return schema.validate(user);
+};
 
 const UserModel = model('User', userSchema)
 
 export {
     UserModel,
-    User
+    User,
+    validateUser,
+    hashPassword
 }
