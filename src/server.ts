02e4bfd13a2bc9323bdb9df4d6aa5f4672bafebe
Zhengyi Zhuo <zhuozhengyi@icloud.com>
2024-05-16 16:12:55 +0800

    Trying add http support

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import IndexRoute from "./routes/index.route";
 import TaskRoute from "./routes/task.route";
 import FileRoute from "./routes/file.route";
 import AuthRoute from "./routes/auth.route";
+import * as https from "https";
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
 const app = new App([
     new IndexRoute(),
@@ -11,4 +16,14 @@ const app = new App([
     new AuthRoute()
 ])
 
-app.listen();
+if (process.env.HTTPS_ENABLED === 'true') {
+    const port = 443;
+    https.createServer({
+        key: process.env.HTTPS_KEY!,
+        cert: process.env.HTTPS_CERT!
+    }, app.getServer()).listen(port, () => {
+        console.log(`App listening on the port ${port}`);
+    });
+} else {
+    app.listen();
+}
