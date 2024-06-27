825f4ca603c10af5abbcacbe41221f3fbd80e2f1
Zhengyi Zhuo <zhuozhengyi@icloud.com>
2024-06-27 21:40:44 +0800

    File Url with host domain name

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -4,24 +4,26 @@ import axios from "axios";
 import { getAudioDurationInSeconds } from 'get-audio-duration'
 
 class FileService {
-    private readonly HOST: string;
-    private readonly PORT: string;
+    // private readonly HOST: string;
+    // private readonly PORT: string;
+    private readonly HOST_URL: string;
 
     constructor() {
-        this.HOST = process.env.HOST!;
-        this.PORT = process.env.PORT!;
+        // this.HOST = process.env.HOST!;
+        // this.PORT = process.env.PORT!;
+        this.HOST_URL = process.env.HOST_URL!;
     }
 
     public imageUrl(filename: string) {
-        return `${this.HOST}:${this.PORT}/file/image/${filename}`;
+        return `${this.HOST_URL}/file/image/${filename}`;
     }
 
     public audioUrl(filename: string) {
-        return `${this.HOST}:${this.PORT}/file/audio/${filename}`;
+        return `${this.HOST_URL}/file/audio/${filename}`;
     }
 
     public imageUrlWithSubDir(subDir: string, filename: string) {
-        return `${this.HOST}:${this.PORT}/file/image${subDir}/${filename}`;
+        return `${this.HOST_URL}/file/image${subDir}/${filename}`;
     }
 
     public getImageList() {
