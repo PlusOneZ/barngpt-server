3d16f580ddada80b611bbf3d981f23c4d972531c
Zhengyi ZHUO <zhuozhengyi@icloud.com>
2024-09-05 22:17:43 +0800

    Fix multiple line user prompts for ig

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -3,9 +3,9 @@
  * Compose a string from a list of prompts
  * @param prompts
  */
-function composePrompts(prompts: any) : string {
+function composePrompts(prompts: any[]) : string {
     return prompts.reduce((prompt : string, v: any) => {
-        return prompt + (v.role === "user") ? v.content : ""
+        return prompt + ((v.role === "user") ? v.content : "")
     }, "")
 }
 
