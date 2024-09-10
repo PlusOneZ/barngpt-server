349bc29bbfb8124862925a4f6b17129108e5f6e3
Zhengyi ZHUO <zhuozhengyi@icloud.com>
2024-09-10 12:27:45 +0800

    Rm dist

--- dist/utils/logging.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = exports.dbLog = void 0;
-const tslog_1 = require("tslog");
-exports.dbLog = new tslog_1.Logger({ name: "Database" });
-exports.log = new tslog_1.Logger({ name: "Server" });
-//# sourceMappingURL=logging.js.map
